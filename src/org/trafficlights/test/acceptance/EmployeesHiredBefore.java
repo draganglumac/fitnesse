d0ae360377c6a1647b50c5fe570a03f28d7a7935
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-07-22 08:43:02 +0100

    Training with Anju and Raffi

---
+++ src/org/trafficlights/test/acceptance/EmployeesHiredBefore.java
@@ -0,0 +1,20 @@
+package org.trafficlights.test.acceptance;
+
+import java.util.List;
+
+import static util.ListUtility.list;
+
+
+/**
+ * Created by dragan on 21/07/2014.
+ */
+public class EmployeesHiredBefore {
+
+    public EmployeesHiredBefore(String date) {
+        // To Do
+    }
+
+    public List<List<List<String>>> query() {
+        return list(list(list("", "")));
+    }
+}
---
+++ src/org/trafficlights/test/acceptance/ShouldIBuyMilk.java
@@ -0,0 +1,23 @@
+package org.trafficlights.test.acceptance;
+
+/**
+ * Created by dragan on 21/07/2014.
+ */
+public class ShouldIBuyMilk {
+
+    public void setCashInWallet(int amount) {
+        // To Do
+    }
+
+    public void setCreditCard(String valid) {
+        // To Do
+    }
+
+    public void setPintsOfMilkRemaining(int pintsRemaining) {
+        // To Do
+    }
+
+    public String goToStore() {
+        return "";
+    }
+}
