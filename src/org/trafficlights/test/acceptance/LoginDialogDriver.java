bf46ab2698cfd44a6939473b454b5683dfc5fcca
Dragan Glumac <dragan.glumac2@bskyb.com>
2014-07-22 11:31:52 +0100

    Scripts and scenarios.

---
+++ src/org/trafficlights/test/acceptance/LoginDialogDriver.java
@@ -0,0 +1,23 @@
+package org.trafficlights.test.acceptance;
+
+/**
+ * Created by dragan on 22/07/2014.
+ */
+public class LoginDialogDriver {
+
+    public LoginDialogDriver(String username, String password) {
+        // To Do
+    }
+
+    public boolean loginWithUsernameAndPassword(String username, String password) {
+        return false;
+    }
+
+    public String loginMessage() {
+        return "something happened";
+    }
+
+    public String numberOfLoginAttempts() {
+        return "Missing";
+    }
+}
